59b945786203fa006e94343f994aef56aa7a708e
Jatu <iceiceitim@gmail.com>
2026-06-21 14:59:53 +0700

    fix Assignment5-2

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -2,27 +2,23 @@
 # /etc/init.d/S99aesdsocket
 
 DAEMON=/usr/bin/aesdsocket
-PIDFILE=/var/run/aesdsocket.pid
 
 case "$1" in
     start)
         echo "Starting aesdsocket..."
         start-stop-daemon --start \
             --exec $DAEMON \
-            --pidfile $PIDFILE \
-            --make-pidfile \
-            --background \
             -- -d
         ;;
     stop)
         echo "Stopping aesdsocket..."
         start-stop-daemon --stop \
             --exec $DAEMON \
-            --pidfile $PIDFILE \
             --signal TERM \
             --oknodo
         ;;
     *)
         echo "Usage: $0 {start|stop}"
         exit 1
+        ;;
 esac
